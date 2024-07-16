2e29af149670392e7b60040817fd667bd89fc725
maddojka <yuriy.soroko@gmail.com>
2024-07-16 22:14:04 +0300

    Soroko task01

--- src/main/java/com/springlessons/orders/task01/PaymentService.java
+++ src/main/java/com/springlessons/orders/task01/PaymentService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Service
 public class PaymentService {
     ProductService productService;
@@ -11,10 +13,10 @@ public class PaymentService {
     UserService userService;
 
 
-    public Mono<PaymentInfo> getPaymentInfo() {
-        Mono<User> monoUser = userService.getUsers();
-        Mono<Trader> monoTrader = traderService.getTraders();
-        Mono<Product> monoProduct = productService.getProducts();
+    public Mono<PaymentInfo> getPaymentInfo(List<Integer> users, List<Integer> traders, List<Integer> products) {
+        Mono<User> monoUser = userService.getUsers(users);
+        Mono<Trader> monoTrader = traderService.getTraders(traders);
+        Mono<Product> monoProduct = productService.getProducts(products);
         Mono<PaymentInfo> paymentInfoMono = Mono.zip(monoTrader, monoUser, monoProduct) // Tuple
                 .flatMap(objects -> {
                     Trader trader = objects.getT1();// cat
